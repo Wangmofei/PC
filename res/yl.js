1a924acb30c33bbf32e30ce18bd1d5ce472568de
mofei <121306@qq.com>
2021-07-28 16:15:05 +0800

    update

--- res/yl.js
+++ res/yl.js
@@ -6,7 +6,7 @@ window.YL = {
     author: 'Yuri2',
     contactInformation: 'yuri2peter@qq.com',
     officialWebsite: 'https://ylui.yuri2.cn',
-    copyrightDetail: 'YLUI已授权本产品使用。YLUI禁止任何未经作者授权的使用、修改、二次发布，包括从本产品中剥离YLUI独立使用等侵权行为，违者将追究法律责任。',
+    copyrightDetail: 'YLUI已授权本产品使用。Mofei禁止任何未经作者授权的使用、修改、二次发布，包括从本产品中剥离YLUI独立使用等侵权行为，违者将追究法律责任。',
     otherStatements: '',
   },
   data: {},
