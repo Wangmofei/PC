ab7a0a10e044581c569f046c164da0006a69176b
mofei <121306@qq.com>
2021-07-28 10:40:50 +0800

    Update

--- res/yl.js
+++ res/yl.js
@@ -1,12 +1,12 @@
 window.YL = {
   info: {
-    softwareName: 'YLUI',
+    softwareName: 'Mofei',
     version: window.yluiVersion,
     iconBtnStart: 'yoast',
-    author: 'Yuri2',
-    contactInformation: 'yuri2peter@qq.com',
-    officialWebsite: 'https://ylui.yuri2.cn',
-    copyrightDetail: 'YLUI已授权本产品使用。YLUI禁止任何未经作者授权的使用、修改、二次发布，包括从本产品中剥离YLUI独立使用等侵权行为，违者将追究法律责任。',
+    author: 'Mofei',
+    contactInformation: '121306@qq.com',
+    officialWebsite: 'web.mofei.wang',
+    copyrightDetail: 'Mofei已授权本产品使用。Mofei禁止任何未经作者授权的使用、修改、二次发布，包括从本产品中剥离YLUI独立使用等侵权行为，违者将追究法律责任。',
     otherStatements: '',
   },
   data: {},
